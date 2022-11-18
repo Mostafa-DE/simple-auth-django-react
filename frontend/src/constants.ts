ea6294f6027c258bcb05cd26c551cb5dcc631af7
mostafa-de <mostafafayyado1@gmail.com>
2022-11-19 00:16:05 +0300

    Move URL to constants file and create type file

---
+++ frontend/src/constants.ts
@@ -0,0 +1,1 @@
+export const URL = "https://knox-auth-django-production.up.railway.app";
---
+++ frontend/src/types.ts
@@ -0,0 +1,8 @@
+export interface User {
+  created: string;
+  email: string;
+  id: number;
+  is_active: boolean;
+  updated: string;
+  username: string;
+}
